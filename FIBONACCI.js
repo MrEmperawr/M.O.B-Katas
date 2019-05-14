60f191fc1717a499a5e692f7d9aa19026e74c547
Marcus M. Kalmár <marcus.kalmar@gmail.com>
2019-05-14 23:13:48 +0200

    Update FIBONACCI.js

--- FIBONACCI.js
+++ FIBONACCI.js
@@ -1,41 +1,42 @@
-let a = 1;
-let b = 1;
-
-for (let i = 0; i < 100; i++) {
-    if (i % 2 === 0) {
-        a = b + a;
-        console.log(a);
-        primeNumber(a);
-    } else {
-        b = a + b;
-        console.log(b);
-        primeNumber(b);
-    }
+function fibonacciPrimes(number, iterations) {
+    let a = 1;
+    let b = 1;
+    let primeArray = [];
+    for (let i = 0; i < iterations; i++) {
+        if (i % 2 === 0) {
+            a = b + a;
+            console.log(a);
+            primeNumber(a);
+            primeArray.push(primeNumber(a));
+            
+        } else {
+            b = a + b;
+            console.log(b);
+            primeNumber(b);
+            primeArray.push(primeNumber(b));
+        }
+    } return primeArray;
 }
 
 function primeNumber (number) {
     if (number === 1) {
-        console.log(number + ' is 1');
+        console.log('1 is the unit number.');
         return;
     }
+    if (number === 2) {
+        return console.log(`${number} is a prime number!`);
+    }
     if (number % 2 === 0) {
-        console.log(number + ' is even');
-        return;
+        return console.log(`${number} is even.`)
     }
     for (let i = number-1; i > 1; i-- ) {
         if (number % i !== 0) {
             if (i === 2) {
-                console.log(number + ' IS A PRIME NUMBER!');
-                break;
+                console.log(`${number} is a prime number!`);
+                return number;
             }
-            //console.log('Tried ' + i + ', moving on to ' + (i-1));
         } else { 
-            console.log(number + ' is NOT a prime number because it is divisible by ' + i);
-            break;
+            return console.log(`${number} is not a prime number, it is divisible by ${i}`);
         }
     }
 }
-
-//primeNumber(795121);
-//795121
-//92837465
